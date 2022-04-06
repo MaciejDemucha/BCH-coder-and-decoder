810885698dd77a1c2b14b6628426fdced04f2e58
Maciej Demucha <72813169+MaciejDemucha@users.noreply.github.com>
2022-04-06 23:14:02 +0200

    wielomian generujący

--- bchcode1.m
+++ bchcode1.m
@@ -1,24 +1,23 @@
 clear;
-%m = 4; % Or choose any positive integer value of m.
-%alph = gf(2,m) % Primitive element in GF(2^m)
 
 x = input("Wpisz długość słowa wejściowego: "); %pobranie od usera dł słowa wejciowego x
 n = input("Wpisz długość słowa kodowego (min. 255): ");
 t = input("Wpisz zdolność korekcyjną t: ");
 m = fix(log2(n+1)); %liczba wielomianów minimalnych (fix -> cz. całkowita)
-k = n - t*m; %cz. informacyjna
+%k = n - t*m; %cz. informacyjna
+k = input("Wpisz długość części informacyjnej: ");
 xWord = randsrc(x, 1, [0 1]); %losowanie słowa wej
 fprintf("Dane wejściowe: "); %wypisanie słowa wej
 fprintf("%d", xWord);
-y = primpoly(m);
-fprintf("m = "); %wypisanie liczby wielomianów minimalnych
-fprintf("%d", m);
-fprintf(y);
+m = 6;
+fprintf("\nm = "); 
+fprintf("%d\n", m);
+[y] = primpoly(m, 'all'); %wypisanie liczby wielomianów minimalnych ciała galois
 
-%lcm()  NWW - potrzebne później do wyliczenia wielomianu generującego
+for i = 1: length(y)-1      %g(x) = NWW(m1(x), m3(x)....)
+    L = lcm(y(i), y(i+1));
+end
+l = dec2bin(L);
+fprintf("%s\n", l);
 
-%m = 4;
-%defaultprimpoly = primpoly(m) % Default primitive poly for GF(16)
-%allprimpolys = primpoly(m,'all') % All primitive polys for GF(16)
-%i1 = isprimitive(25) % Can 25 be the prim_poly input in gf(...)?
-%i2 = isprimitive(21) % Can 21 be the prim_poly input in gf(...)?
+[genpoly,t] = bchgenpoly(n,k) %porównanie z gotową funkcją
